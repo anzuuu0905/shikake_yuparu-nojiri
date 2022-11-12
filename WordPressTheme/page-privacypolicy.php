1df9d39b8b36aed9d85494de322ac853fe4e5397
anzuuu0905 <anzuuu@yahoo.co.jp>
2022-11-13 06:21:35 +0900

    プライバシーポリシー作成

---
+++ WordPressTheme/page-privacypolicy.php
@@ -0,0 +1,58 @@
+<?php get_header(); ?>
+
+<main class="main grid__content">
+      <section class="subpage-head__common">
+        <div class="subpage-head__text">
+          <h1 class="subpage__title">プライバシーポリシー</h1>
+          <span class="subpage__subtitle">Privacy Policy</span>
+        </div>
+      </section>
+      <!-- パンくずリスト -->
+      <?php get_template_part('parts/breadcrumb'); ?>
+
+      <section class="privacy privacy_block">
+        <div class="inner privacy__inner">
+          <div class="privacy__content">
+            <h2 class="privacy__title">法令の遵守</h2>
+            <p class="privacy__text">ハーメックのじり株式会社（以下、当社という）は、個人情報（特定の個人を識別できるもの、以下も同様とします。）を取り扱う際に、個人情報の保護に関する法律をはじめ個人情報保護に関する諸法令、および主務大臣のガイドラインに定められた義務、並びに本ポリシーを遵守します。</p>
+          </div>
+          <div class="privacy__content">
+            <h2 class="privacy__title">利用目的の範囲内での利用</h2>
+            <p class="privacy__text">当社は、利用目的をできる限り特定したうえ、あらかじめご本人の同意を得た場合、および法令により例外として認められた場合を除き、明示または公表した利用目的の範囲内でのみ個人情報を取り扱います。</p>
+          </div>
+          <div class="privacy__content">
+            <h2 class="privacy__title">利用目的等の明示・公表</h2>
+            <p class="privacy__text">当社は、あらかじめ利用目的、共同利用者の範囲、お問い合わせ窓口等の必要な情報を明示し、同意を得たうえで個人情報を取得するよう努めます。なお、当社は、お取り引きやお問い合わせに関する内容を記録あるいは録音させていただく場合がありますが、取得した情報はご本人のご要望に適切かつ迅速に対応するためにのみ取り扱います。</p>
+          </div>
+          <div class="privacy__content">
+            <h2 class="privacy__title">未成年者の個人情報</h2>
+            <p class="privacy__text">当社は、原則として15歳未満のお子様から個人情報を取得することはありません。15歳未満のお子様から個人情報を取得する可能性がある場合、保護者のご同意をいただきご提供下さるよう明示したうえで取得する等、未成年者の個人情報の取り扱いに関し、特別の配慮を行います。</p>
+          </div>
+          <div class="privacy__content">
+            <h2 class="privacy__title">安全管理措置</h2>
+            <p class="privacy__text">当社は、お預かりした個人情報を利用目的の範囲内で正確・最新の内容に保つよう努め、不正なアクセス、改ざん、漏えいなどから守るべく、現時点での技術水準に合わせた必要かつ適切な安全管理措置を講じます。</p>
+          </div>
+          <div class="privacy__content">
+            <h2 class="privacy__title">委託先の監督</h2>
+            <p class="privacy__text">当社は、お預かりした個人情報の処理を利用目的の範囲内で第三者に委託する場合があります。これらの第三者は、十分な個人情報のセキュリティ水準にあることを確認の上選定し、契約等を通じて、必要かつ適切な監督を行います。</p>
+          </div>
+          <div class="privacy__content">
+            <h2 class="privacy__title">第三者への提供</h2>
+            <p class="privacy__text">当社は、法令により例外として認められた場合を除き、ご本人の同意を得ることなく、取得時に明示した共同利用者以外の第三者に個人情報を提供しません。なお、当社は、オプトアウト制度（個人情報の保護に関する法律第23条第2項）を利用して、ご本人の認識なく個人情報を第三者に提供しません。</p>
+          </div>
+          <div class="privacy__content">
+            <h2 class="privacy__title">開示等の求め</h2>
+            <p class="privacy__text">当社は、ご本人が自らの個人情報の開示、訂正、商品やサービスの紹介の停止、または消去などを希望される場合、ご本人であることを確認したうえで、法令の規定に基づき、すみやかに対応します。お客様が個人情報を提供された各窓口にご請求ください。</p>
+          </div>
+          <div class="privacy__content">
+            <h2 class="privacy__title">苦情への対応</h2>
+            <p class="privacy__text">当社は、取り扱う個人情報につき、ご本人からの苦情に対し迅速かつ適切に取組み、そのための体制の整備を行います。</p>
+          </div>
+          <div class="privacy__content">
+            <h2 class="privacy__title">体制の継続的改善</h2>
+            <p class="privacy__text">当社は、お預かりした個人情報を適切に取り扱うために、内部規程の整備、適正な内部監査の実施等を通じて、本ポリシーの見直しを含めた体制の継続的強化・改善に努めます。</p>
+          </div>
+        </div>
+      </section>
+
+<?php get_footer(); ?>
